df91bde3439db2ccca10be6cd5642d0584ed8113
CarlBarace <80297038+CarlBarace@users.noreply.github.com>
2021-03-18 08:15:00 +0800

    Updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 
 app.get("/",function(req,res){
   res.send("IT WORKED")
-
+})
+app.get("/new",function(req,res){
+  res.send("IT ALSO WORKED")
 })
 
 app.listen(process.env.PORT || 5000)
